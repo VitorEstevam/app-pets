e9555670060faff7ebf8acca856dcc546aa88350
vitor estevam <vitorestevam02@gmail.com>
2021-12-11 18:13:10 -0300

    new colors

--- lib/consts/theme.dart
+++ lib/consts/theme.dart
@@ -1,5 +1,19 @@
 import 'package:flutter/material.dart';
 
 ThemeData theme = ThemeData(
-  primarySwatch: Colors.blue,
+  primarySwatch: const MaterialColor(
+    0xFFFF5967,
+    <int, Color>{
+      50: Color.fromRGBO(255, 89, 103, .1),
+      100: Color.fromRGBO(255, 89, 103, .2),
+      200: Color.fromRGBO(255, 89, 103, .3),
+      300: Color.fromRGBO(255, 89, 103, .4),
+      400: Color.fromRGBO(255, 89, 103, .5),
+      500: Color.fromRGBO(255, 89, 103, .6),
+      600: Color.fromRGBO(255, 89, 103, .7),
+      700: Color.fromRGBO(255, 89, 103, .8),
+      800: Color.fromRGBO(255, 89, 103, .9),
+      900: Color.fromRGBO(255, 89, 103, 1),
+    },
+  ),
 );
