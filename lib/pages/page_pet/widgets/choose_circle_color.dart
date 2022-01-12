1c48c816e72df2fd317997375dd3cd8ed9ef8828
Vitor Estevam <vitorestevam02@gmail.com>
2022-01-12 10:47:37 -0300

    fix page_create_pet

--- lib/pages/page_pet/widgets/choose_circle_color.dart
+++ lib/pages/page_pet/widgets/choose_circle_color.dart
@@ -1,23 +1,28 @@
-
 import 'package:app_pets/widgets/image_loader.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChooseCircleColor extends StatelessWidget {
-
   String imgUrl;
   Color color = Colors.white;
-  void Function(String, Color) action; 
+  void Function(String, Color) action;
 
   ChooseCircleColor(this.imgUrl, this.color, this.action);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: OutlinedButton(
-        child: Image.asset(imgUrl, width: 100, height: 100, fit: BoxFit.fill,),
-        onPressed: (){   action(imgUrl, color); }
-      )
-    ); 
+    return OutlinedButton(
+        clipBehavior: Clip.antiAlias,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<OutlinedBorder>(
+              const CircleBorder()),
+        ),
+        child: Image.asset(
+          imgUrl,
+          fit: BoxFit.cover,
+        ),
+        onPressed: () {
+          action(imgUrl, color);
+        });
   }
 }
--- lib/pages/page_pet/widgets/choose_circle_icon.dart
+++ lib/pages/page_pet/widgets/choose_circle_icon.dart
@@ -1,24 +1,32 @@
-
 import 'package:app_pets/widgets/image_loader.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ChooseCircleIcon extends StatelessWidget {
-
   String imgUrl;
-  void Function(String) action; 
-  Widget ?button;
- 
+  void Function(String) action;
+  Widget? button;
+  bool isSelected;
 
-  ChooseCircleIcon(this.imgUrl, this.action);
+  ChooseCircleIcon(this.imgUrl, this.action,
+      {Key? key, this.isSelected = false})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: OutlinedButton(
-        child: Image.asset(imgUrl, width: 175, height: 100, fit: BoxFit.fill,),
-        onPressed: (){   action(imgUrl); }
-      )
-    ); 
+    return OutlinedButton(
+        clipBehavior: Clip.antiAlias,
+        style: ButtonStyle(
+          shape: MaterialStateProperty.all<OutlinedBorder>(const CircleBorder()),
+        ),
+        child: Image.asset(
+          imgUrl,
+          width: 100,
+          height: 100,
+          fit: BoxFit.cover,
+        ),
+        onPressed: () {
+          action(imgUrl);
+        });
   }
 }
