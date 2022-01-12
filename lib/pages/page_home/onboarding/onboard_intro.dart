3a245809e55d7ff4fd4a97cc7ece696188423dc2
Jefferson Jales <jeffersonjales@hotmail.com>
2022-01-11 22:44:25 -0300

    Refactoring intro based on onboard lib

--- lib/pages/page_home/onboarding/onboard_intro.dart
+++ lib/pages/page_home/onboarding/onboard_intro.dart
@@ -1,72 +1,92 @@
+import 'package:app_pets/pages/tab_bar_handler.dart';
+import 'package:app_pets/stores/example/store_global.dart';
+import 'package:app_pets/widgets/image_loader.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:onboarding/onboarding.dart';
+import 'package:provider/provider.dart';
 
 class OnboardingIntro extends StatelessWidget {
-
-  final List<PageModel> onboardingPagesList = []; 
   
+  final List<PageModel> onboardingPagesList = []; 
+ 
   PageModel _create_page_model (String textTitle, String textBody, String img_path_way){
    return PageModel(
       widget: Column(
-              children: [
-                Container(
-                    width: double.infinity,
-                    child: Text(textTitle, style: pageTitleStyle)),
-                Container(
-                  width: double.infinity,
-                  child: Text(
-                    textBody,
-                    style: pageInfoStyle,
-                  ),
+        children: [
+          Container(
+              width: double.infinity,
+              padding: EdgeInsets.only(bottom: 15.0),
+              child: Text(
+                textTitle, 
+                style: TextStyle(color: Colors.black, fontSize: 28),
                 ),
-                Container(
-                    padding: EdgeInsets.only(bottom: 45.0),
-                    child: Image.asset(img_path_way,
-                        color: pageImageColor)),
-              ],
+              ),
+          Container(
+            width: double.infinity,
+            child: Text(
+              textBody,
+              style: TextStyle(color: Colors.black, fontSize: 16),
             ),
+          ),
+          Expanded(
+            child: ImageLoader(img_path_way), 
+          ),
+        ],
+      ),
     );
   }
-
+  
   OnboardingIntro(){
     onboardingPagesList.add(_create_page_model( "Bem-vindo(a) ao {app}!",  
                                                 "Feito com carinho para que você possa dedicar mais tempo ao seu bichinho de estimação!", 
-                                                "assets/intro_images/Pag1_pets.png"));
+                                                'lib/assets/Pag1_pets.png'));
   
     onboardingPagesList.add(_create_page_model( "Melhore os laços com seu pet!",  
                                                 "Cumprir com todas as responsabilidades do seu pet não só é importante para a saúde dele(a) como ajuda melhorar os laços entre o bichinho e dono(a)! ", 
-                                                "assets/intro_images/Pag2_pets.png"));
+                                                "lib/assets/Pag2_pets.png"));
                                                 
     onboardingPagesList.add(_create_page_model( "Sobre o {app}",  
                                                 "Lembre de todas as responsabilidades para seu pet gerenciando suas tarefas!", 
-                                                "assets/intro_images/Pag2_pets.png"));
+                                                "lib/assets/Pag3_pets.png"));
 
     onboardingPagesList.add(_create_page_model( "Então vamos começar!",  
                                                 "Adicione seu primeiro pet! Selecione um ícone e uma cor para representá-lo ao usar o aplicativo.", 
-                                                "assets/intro_images/Pag2_pets.png"));
+                                                "lib/assets/Pag4_pets.png"));
   }
 
   @override
   Widget build(BuildContext context) {
+    var _store_global = Provider.of<StoreGlobal>(context);
+    
     return Scaffold(
       body: Onboarding( 
+        background: Colors.white,
         isSkippable: false,
+
         proceedButtonStyle: ProceedButtonStyle(
-            proceedButtonRoute: (context) {
-              return Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => Container(),
-                ),
-                (route) => false,
-              );
-            }),
+          proceedButtonColor: Color.fromRGBO(255, 89, 103, 1),
+          proceedpButtonText: Text("Avançar", style: TextStyle(color: Colors.white),),
+          proceedButtonRoute: (context) {
+            
+            _store_global.tutorialDone = true; // @TODO: SAVE THIS
+
+            return Navigator.pushAndRemoveUntil(
+              context,
+              MaterialPageRoute(
+                builder: (context) => TabBarHandler(),
+              ),
+              (route) => false, 
+            );
+          }),
         pages: onboardingPagesList,
         indicator: Indicator(
+          activeIndicator: ActiveIndicator(color: Colors.grey, ),
+          closedIndicator: ClosedIndicator(color: Colors.grey, ),
           indicatorDesign: IndicatorDesign.polygon(
             polygonDesign: PolygonDesign(
               polygon: DesignType.polygon_circle,
+
             ),
           ),
         ),
--- lib/stores/example/store_global.dart
+++ lib/stores/example/store_global.dart
@@ -1,4 +1,8 @@
 import 'package:app_pets/classes/pet.dart';
+import 'package:app_pets/pages/page_home/onboarding/onboard_intro.dart';
+import 'package:app_pets/pages/tab_bar_handler.dart';
+import 'package:flutter/cupertino.dart';
+
 import 'package:mobx/mobx.dart';
 // run builder on cmd to generate the code
 
@@ -8,8 +12,19 @@ class StoreGlobal = _StoreGlobal with _$StoreGlobal;
 // Create the class
 abstract class _StoreGlobal with Store {
   @observable
-  bool tutorialDone = false;
+  bool tutorialDone = true;
 
   @observable
   ObservableList<Pet> pets = ObservableList<Pet>.of([]);
+
+  Widget starting_app_route(){
+    if(!tutorialDone) return OnboardingIntro();
+    //else if(pets.isEmpty) return OnboardingIntro(); // @TODO CRIAÇÃO DE PETS
+    else return TabBarHandler();
+  }
+
+  @action 
+  void addNewPet(Pet){
+    pets.add(Pet);    
+  }
 }
