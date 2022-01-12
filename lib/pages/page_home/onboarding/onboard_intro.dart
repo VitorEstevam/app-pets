24e1970c510368b4b6cc15b21a75b73b7d91d1c4
Vitor Estevam <vitorestevam02@gmail.com>
2022-01-12 13:46:19 -0300

    change texts from onboarding's buttons

--- lib/pages/page_home/onboarding/onboard_intro.dart
+++ lib/pages/page_home/onboarding/onboard_intro.dart
@@ -9,101 +9,6 @@ import 'package:intro_slider/slide_object.dart';
 import 'package:onboarding/onboarding.dart';
 import 'package:provider/provider.dart';
 
-// class OnboardingIntro extends StatelessWidget {
-//   final List<PageModel> onboardingPagesList = [];
-
-//   PageModel _create_page_model(
-//       String textTitle, String textBody, String img_path_way) {
-//     return PageModel(
-//       widget: Column(
-//         children: [
-//           Container(
-//             width: double.infinity,
-//             padding: const EdgeInsets.only(bottom: 15.0),
-//             child: Text(
-//               textTitle,
-//               style: const TextStyle(color: Colors.black, fontSize: 28),
-//             ),
-//           ),
-//           Container(
-//             width: double.infinity,
-//             child: Text(
-//               textBody,
-//               style: const TextStyle(color: Colors.black, fontSize: 16),
-//             ),
-//           ),
-//           Expanded(
-//             child: ImageLoader(img_path_way),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-
-//   OnboardingIntro() {
-//     onboardingPagesList.add(_create_page_model(
-//         "Bem-vindo(a) ao {app}!",
-//         "Feito com carinho para que você possa dedicar mais tempo ao seu bichinho de estimação!",
-//         'lib/assets/Pag1_pets.png'));
-
-//     onboardingPagesList.add(_create_page_model(
-//         "Melhore os laços com seu pet!",
-//         "Cumprir com todas as responsabilidades do seu pet não só é importante para a saúde dele(a) como ajuda melhorar os laços entre o bichinho e dono(a)! ",
-//         "lib/assets/Pag2_pets.png"));
-
-//     onboardingPagesList.add(_create_page_model(
-//         "Sobre o {app}",
-//         "Lembre de todas as responsabilidades para seu pet gerenciando suas tarefas!",
-//         "lib/assets/Pag3_pets.png"));
-
-//     onboardingPagesList.add(_create_page_model(
-//         "Então vamos começar!",
-//         "Adicione seu primeiro pet! Selecione um ícone e uma cor para representá-lo ao usar o aplicativo.",
-//         "lib/assets/Pag4_pets.png"));
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     var _store_global = Provider.of<StoreGlobal>(context);
-
-//     return Scaffold(
-//         body: Onboarding(
-//       background: Colors.white,
-//       isSkippable: false,
-//       proceedButtonStyle: ProceedButtonStyle(
-//           proceedButtonColor: const Color.fromRGBO(255, 89, 103, 1),
-//           proceedpButtonText: const Text(
-//             "Avançar",
-//             style: const TextStyle(color: Colors.white),
-//           ),
-//           proceedButtonRoute: (context) {
-//             _store_global.tutorialDone = true; // @TODO: SAVE THIS
-
-//             return Navigator.pushAndRemoveUntil(
-//               context,
-//               MaterialPageRoute(
-//                 builder: (context) => CreatePetIntro(),
-//               ),
-//               (route) => false,
-//             );
-//           }),
-//       pages: onboardingPagesList,
-//       indicator: Indicator(
-//         activeIndicator: const ActiveIndicator(
-//           color: Colors.grey,
-//         ),
-//         closedIndicator: const ClosedIndicator(
-//           color: Colors.grey,
-//         ),
-//         indicatorDesign: IndicatorDesign.polygon(
-//           polygonDesign: PolygonDesign(
-//             polygon: DesignType.polygon_circle,
-//           ),
-//         ),
-//       ),
-//     ));
-//   }
-// }
 
 class OnboardingIntro extends StatefulWidget {
   const OnboardingIntro({Key? key}) : super(key: key);
@@ -193,15 +98,15 @@ class _OnboardingIntroState extends State<OnboardingIntro> {
 
       // Prev button
       showPrevBtn: true,
-      renderPrevBtn: myButton("VOLTAR"),
+      renderPrevBtn: myButton("Voltar"),
       prevButtonStyle: myButtonStyle(),
 
       // Next button
-      renderNextBtn: myButton("AVANÇ12312312312AR"),
+      renderNextBtn: myButton("Avançar"),
       nextButtonStyle: myButtonStyle(),
 
       // Done button
-      renderDoneBtn: myButton("PRONTO"),
+      renderDoneBtn: myButton("Pronto"),
       doneButtonStyle: myButtonStyle(),
       onDonePress: (){
         _store_global.tutorialDone = true; // @TODO: SAVE THIS
