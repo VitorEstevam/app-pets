e4cc4aa385d2d2ad607987fa0f30e3283a4e4a8f
Vitor Estevam <vitorestevam02@gmail.com>
2022-01-12 10:18:27 -0300

    fix pet icon on home

--- lib/pages/page_home/subpages/page_home/widgets/pet_picture.dart
+++ lib/pages/page_home/subpages/page_home/widgets/pet_picture.dart
@@ -7,57 +7,42 @@ class PetPicture extends StatelessWidget {
   const PetPicture({
     Key? key,
   }) : super(key: key);
-  
-  
 
   @override
   Widget build(BuildContext context) {
     var _store_global = Provider.of<StoreGlobal>(context);
-    
-    return Column(
-      children: [
-        Stack(
-          alignment: Alignment.topCenter,
-          children: [
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.cover,
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(_store_global.pets[0].colorIconUrl),
-            ),  
-          ),
-          
-          Expanded(
-            child: FittedBox(
-              fit: BoxFit.cover ,
-              alignment: Alignment.bottomCenter,
-              child: Image.asset(_store_global.pets[0].petIconUrl),
-            ),
-          ),
-        ],
 
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+    return Column(children: [
+      Expanded(
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            Text(
-              _store_global.pets[0].name, //"Luke", /// @TODO Pegar nome do pet em destaque
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            IconButton(
-              splashRadius:20.0,
-              onPressed: () {},
-              icon: Icon(
-                Icons.edit,
-                color: Theme.of(context).primaryColor,
-              ),
-            )
+            Image.asset(_store_global.pets[0].colorIconUrl),
+            Image.asset(_store_global.pets[0].petIconUrl),
           ],
-        )
-      ]
-    );
+        ),
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            _store_global
+                .pets[0].name, //"Luke", /// @TODO Pegar nome do pet em destaque
+            style: const TextStyle(
+              fontSize: 24,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          IconButton(
+            splashRadius: 20.0,
+            onPressed: () {},
+            icon: Icon(
+              Icons.edit,
+              color: Theme.of(context).primaryColor,
+            ),
+          )
+        ],
+      )
+    ]);
   }
 }
