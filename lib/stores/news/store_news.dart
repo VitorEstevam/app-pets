eab4993eb2af908846db38588a70b50e8bac82ef
Vitor Estevam <vitorestevam02@gmail.com>
2022-01-29 02:02:53 -0300

    add news

--- lib/stores/news/store_news.dart
+++ lib/stores/news/store_news.dart
@@ -16,17 +16,21 @@ abstract class _StoreNews with Store {
   @observable
   ObservableList<News> news = ObservableList<News>.of([
     News(
+        title: "Como acostumar seu cachorro com a coleira",
+        header: "lib/assets/news/news_passeio.jpg",
+        text:
+            "Passear com seu pet pode ser desafiador. Alguns pets não estão muito acostumados a esta prática, e acabam puxando a coleira ou ficando muito agitados.\nMuitos pets não gostam da coleira porque não estão acostumados com ela. A técnica para evitar que o cachorro puxe a guia durante o passeio é familiarizar seu amigo com a coleira.\nComece colocando a coleira em seu pet em momentos dentro de casa. Deixe-o brincar um pouco, solto, para se acostumar à sensação da coleira no pescoço.\nDepois, coloque a guia e passeie em um local familiar, como dentro de casa ou no jardim. Quando perceber que seu amigo está passeando tranquilamente, já acostumado com a coleira, é hora de ir para fora!",
+        taskTitle: "Passear com o cachorro"),
+    News(
+        title: "Fortaleza promove Dia “D” de vacinação contra raiva para cães e gatos neste sábado",
+        header: "lib/assets/news/news_vacina.jpg",
+        text:
+            "A Prefeitura de Fortaleza inicia, por meio da Secretaria Municipal da Saúde (SMS), a campanha de vacinação contra raiva animal e promove, neste sábado (06/11), o Dia “D”. Serão 227 pontos de vacinação, incluindo as Unidades de Vigilância de Zoonoses (UVZ), com atendimento das 8h às 16h, para cães e gatos a partir de três meses de idade.\nO Ministério da Saúde preconiza que 80% dos cães sejam vacinados, anualmente, na cidade. Fortaleza alcançou esta meta nos últimos dois anos. Os 20% restantes equivalem ao número de animais abaixo da idade mínima ou que já foram imunizados em clínicas veterinárias particulares. Não há metas preconizadas para gatos pelo MS. Em números absolutos, a meta é vacinar cerca de 440 mil animais, entre cães e gatos, este ano.\nPara realizar a imunização dos seus animais de estimação, os tutores devem levar a carteira de vacinação do animal. Quem não possui o documento recebe a carteirinha no ato da vacinação, com o registro da aplicação.",
+        taskTitle: "Passear com o cachorro"),
+    News(
         title: "Comida ou ração: você sabe qual a melhor opção para o seu pet?",
-        header: "lib/assets/newsHeader.jpg",
-        text: lorem,
-        taskTitle: "Levar para vacinar"),
-    News(
-        title: "teste2",
-        header: "lib/assets/newsHeader.jpg",
-        text: lorem),
-    News(
-        title: "teste3",
-        header: "lib/assets/newsHeader.jpg",
-        text: lorem)
+        header: "lib/assets/news/news_comida.jpg",
+        text:
+            "Enquanto alguns tutores preferem dar alimentos naturais aos pets, outros optam apenas pela ração tradicional. De acordo com a médica veterinária Maria Cecilia de Lima Rorig, antes de mudar a dieta do animal, é necessário pensar nas necessidades de cada um dos peludinhos.\nAssim, se você quer começar a substituir a ração por comida ‘de gente’, a médica explica que é possível, porém, é muito importante procurar um profissional especializado, como um nutricionista veterinário, e conferir o que ele indica para o seu bichinho.\n“Muitas vezes, essas alimentações naturais já são vendidas prontas, congeladas. Não é a comida caseira, que sai direto do nosso prato, com tempero”, diz. Ela ainda complementa esclarecendo que o tutor pode adquirir essa dieta pronta ou então aprender a preparar esse alimento especialmente para a necessidade do animal."),
   ]);
 }
