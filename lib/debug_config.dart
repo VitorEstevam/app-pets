0a205a87e30ab25882f4cae77f42bfdf823dc1a8
Vitor Estevam <vitorestevam02@gmail.com>
2022-01-29 18:01:30 -0300

    better mocks

--- lib/debug_config.dart
+++ lib/debug_config.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:app_pets/classes/tasks/task_unique.dart';
 import 'package:app_pets/classes/tasks/task_weekly.dart';
 import 'package:app_pets/consts/utils.dart';
@@ -8,38 +10,38 @@ import 'package:provider/provider.dart';
 import 'classes/pet.dart';
 
 int petIndex = 0;
-
+bool added = false;
 void debugOptions(BuildContext context) {
+  if (added) return;
+  // added = true;
+
   var pet;
   var task;
-
+  var json;
+  var index;
   //---- luke
-  pet = Pet("Luke $petIndex", "lib/assets/pets/DOG.png", Colors.green);
+  index = Provider.of<StorePets>(context, listen: false).pets.indexWhere(
+        (pet) => pet.name == "Luke",
+      );
+  if (index > -1) {
+    Provider.of<StorePets>(context, listen: false).pets.removeAt(index);
+  }
+
+  json =
+      "{\"name\":\"Luke\",\"petIconUrl\":\"lib\/assets\/pets\/DOG.png\",\"color\":\"Color(0xffff8a65)\",\"tasks\":[{\"title\":\"passear\",\"subTasks\":[{\"dateToDo\":\"2022-01-28 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-28 18:53:35.148738\",\"weekdays\":[3,5]},{\"title\":\"dar banho\",\"subTasks\":[{\"dateToDo\":\"2022-01-24 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-28 18:53:35.142753\"}]}";
+  pet = Pet.fromJson(jsonDecode(json));
   Provider.of<StorePets>(context, listen: false).addNewPet(pet);
-
-  task = TaskUnique("dar banho", pet, DateTime(2022, 01, 24));
-  Provider.of<StorePets>(context, listen: false).addNewTaskToPet(pet, task);
-
-  task = TaskWeekly("passear", pet, [3, 5]);
-  Provider.of<StorePets>(context, listen: false).addNewTaskToPet(pet, task);
 
   //---- zelda
-  pet = Pet("Zelda $petIndex", "lib/assets/pets/CAT.png", Colors.blue);
+  index = Provider.of<StorePets>(context, listen: false).pets.indexWhere(
+        (pet) => pet.name == "Zelda",
+      );
+  if (index > -1) {
+    Provider.of<StorePets>(context, listen: false).pets.removeAt(index);
+  }
+  json =
+      "{\"name\":\"Zelda\",\"petIconUrl\":\"lib\/assets\/pets\/CAT.png\",\"color\":\"MaterialColor(primary value: Color(0xff2196f3))\",\"tasks\":[{\"title\":\"passear\",\"subTasks\":[{\"dateToDo\":\"2022-01-31 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-28 00:00:00.000\",\"done\":\"true\"},{\"dateToDo\":\"2022-01-25 00:00:00.000\",\"done\":\"true\"},{\"dateToDo\":\"2022-01-24 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-21 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-18 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-17 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-14 00:00:00.000\",\"done\":\"true\"},{\"dateToDo\":\"2022-01-11 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-10 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-07 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-04 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-03 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-01 00:00:00.000\",\"weekdays\":[1,2,5]},{\"title\":\"dar banho\",\"subTasks\":[{\"dateToDo\":\"2022-02-01 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-29 20:46:06.959041\"}]}";
+  pet = Pet.fromJson(jsonDecode(json));
   Provider.of<StorePets>(context, listen: false).addNewPet(pet);
-
-  task = TaskUnique("dar banho", pet, DateTime(2022, 02, 01));
-  Provider.of<StorePets>(context, listen: false).addNewTaskToPet(pet, task);
-
-  var t2 = TaskWeekly("passear", pet, [1, 2, 5]);
-  t2.subTasks = [];
-  t2.startDate = DateTime(2022, 01, 01);
-  t2.updateSubTasks();
-  t2.subTasks[7].done = true;
-  t2.subTasks[2].done = true;
-  t2.subTasks[1].done = true;
-  Provider.of<StorePets>(context, listen: false).addNewTaskToPet(pet, t2);
-
-  petIndex += 1;
-
   saveState(context);
 }
