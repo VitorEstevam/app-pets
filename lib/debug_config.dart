6a50609ddef940a37d21ea683675544b352e0f1d
Vitor Estevam <vitorestevam02@gmail.com>
2022-02-02 15:52:09 -0300

    better debug

--- lib/debug_config.dart
+++ lib/debug_config.dart
@@ -15,10 +15,11 @@ void debugOptions(BuildContext context) {
   if (added) return;
   // added = true;
 
-  var pet;
+  Pet pet;
   var task;
   var json;
-  var index;
+  int index;
+
   //---- luke
   index = Provider.of<StorePets>(context, listen: false).pets.indexWhere(
         (pet) => pet.name == "Luke",
@@ -28,7 +29,7 @@ void debugOptions(BuildContext context) {
   }
 
   json =
-      "{\"name\":\"Luke\",\"petIconUrl\":\"lib\/assets\/pets\/DOG.png\",\"color\":\"Color(0xffff8a65)\",\"tasks\":[{\"title\":\"passear\",\"subTasks\":[{\"dateToDo\":\"2022-01-28 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-28 18:53:35.148738\",\"weekdays\":[3,5]},{\"title\":\"dar banho\",\"subTasks\":[{\"dateToDo\":\"2022-01-24 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-28 18:53:35.142753\"}]}";
+      "{\"name\": \"Luke\",\"petIconUrl\": \"lib\/assets\/pets\/DOG3.png\",\"color\": \"Color(0xffff8a65)\",\"tasks\": [{\"title\": \"passear\",\"subTasks\": [{\"dateToDo\": \"2022-02-01 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-31 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-30 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-29 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-28 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-27 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-26 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-25 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-24 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-23 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-22 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-21 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-20 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-19 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-18 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-17 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-16 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-15 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-14 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-13 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-12 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-11 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-10 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-09 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-08 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-07 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-06 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-05 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-04 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-03 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-02 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-01 00:00:00.000\",\"done\": \"true\"}],\"startDate\": \"2022-01-01 18:53:35.148738\",\"weekdays\": [1,2,3,4,5,6,7]},{\"title\": \"Retorno veterin\u00e1rio\",\"subTasks\": [{\"dateToDo\": \"2022-01-24 00:00:00.000\",\"done\": \"false\"}],\"startDate\": \"2022-01-01 18:53:35.142753\"}]}";
   pet = Pet.fromJson(jsonDecode(json));
   Provider.of<StorePets>(context, listen: false).addNewPet(pet);
 
@@ -40,8 +41,9 @@ void debugOptions(BuildContext context) {
     Provider.of<StorePets>(context, listen: false).pets.removeAt(index);
   }
   json =
-      "{\"name\":\"Zelda\",\"petIconUrl\":\"lib\/assets\/pets\/CAT.png\",\"color\":\"MaterialColor(primary value: Color(0xff2196f3))\",\"tasks\":[{\"title\":\"passear\",\"subTasks\":[{\"dateToDo\":\"2022-01-31 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-28 00:00:00.000\",\"done\":\"true\"},{\"dateToDo\":\"2022-01-25 00:00:00.000\",\"done\":\"true\"},{\"dateToDo\":\"2022-01-24 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-21 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-18 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-17 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-14 00:00:00.000\",\"done\":\"true\"},{\"dateToDo\":\"2022-01-11 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-10 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-07 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-04 00:00:00.000\",\"done\":\"false\"},{\"dateToDo\":\"2022-01-03 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-01 00:00:00.000\",\"weekdays\":[1,2,5]},{\"title\":\"dar banho\",\"subTasks\":[{\"dateToDo\":\"2022-02-01 00:00:00.000\",\"done\":\"false\"}],\"startDate\":\"2022-01-29 20:46:06.959041\"}]}";
+      "{\"name\": \"Zelda\",\"petIconUrl\": \"lib\/assets\/pets\/CAT.png\",\"color\": \"Color(0xff2196f3)\",\"tasks\": [{\"title\": \"Ir na pracinha\",\"subTasks\": [{\"dateToDo\": \"2022-02-04 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-02-01 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-31 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-28 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-25 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-24 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-21 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-18 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-17 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-14 00:00:00.000\",\"done\": \"true\"},{\"dateToDo\": \"2022-01-11 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-10 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-07 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-04 00:00:00.000\",\"done\": \"false\"},{\"dateToDo\": \"2022-01-03 00:00:00.000\",\"done\": \"false\"}],\"startDate\": \"2022-01-01 00:00:00.000\",\"weekdays\": [1,2,5]},{\"title\": \"Rem\u00e9dio\",\"subTasks\": [{\"dateToDo\": \"2022-02-02 00:00:00.000\",\"done\": \"false\"}],\"startDate\": \"2022-01-29 20:46:06.959041\"}]}";
   pet = Pet.fromJson(jsonDecode(json));
   Provider.of<StorePets>(context, listen: false).addNewPet(pet);
+
   saveState(context);
 }
